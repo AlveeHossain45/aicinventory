0c629328f21041b6a80ce97a451324c43e88c819
AlveeHossain45 <alveehossain042004@gmail.com>
2025-09-17 17:40:14 +0300

    w

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,8 @@ const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <HashRouter>
+      {/* The future prop is added here to opt-in to future features and remove warnings */}
+      <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <AuthProvider>
           <App />
         </AuthProvider>
